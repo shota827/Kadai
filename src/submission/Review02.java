f191fa845097e705afe1bde71592ffe492534f65
shotsu <shotsu@shotsu>
2024-04-01 18:18:33 +0900

    04.制御構文の課題修正

--- src/submission/Review02.java
+++ src/submission/Review02.java
@@ -4,12 +4,20 @@ public class Review02 {
 
 	public static void main(String[] args) {
 		for (int i = 1; i <= 100; i++) {
+
+			// 3かつ5で割り切れる場合
 			if (i % 3 == 0 && i % 5 == 0) {
 				System.out.println("FizzBuzz");
+
+				// 3で割り切れる場合
 			} else if (i % 3 == 0) {
 				System.out.println("Fizz");
+
+				// 5で割り切れる場合
 			} else if (i % 5 == 0) {
 				System.out.println("Buzz");
+
+				// 上記以外の場合
 			} else {
 				System.out.println(i);
 			}
